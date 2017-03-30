7ac81a3aa465bb5857b078aff13bbd96c35acc1c
Jomel Imperio <jomel@insynchq.com>
2017-03-30 18:15:39 +0800

    Remove ignored files

--- pyobjc-framework-AddressBook/build/lib.macosx-10.10-intel-2.7/PyObjCTest/test_abimageloading.py
+++
@@ -1,9 +0,0 @@
-from PyObjCTools.TestSupport import *
-import AddressBook
-
-class TestABImageLoading (TestCase):
-    def test_methods(self):
-        self.assertResultIsBOOL(AddressBook.ABPerson.setImageData_)
-
-if __name__ == "__main__":
-    main()
